b45e32138e418d52b953ba0f656fbb1bb7ab17ca
Sagar Sapkota <spkt.sagar@gmail.com>
2023-07-09 22:55:26 +0545

    Fix docker ecr auth issue

--- image/build_and_push.sh
+++ image/build_and_push.sh
@@ -6,7 +6,9 @@ cd "$(dirname "$0")"
 
 export AWS_PAGER=""
 
-# Login
+# Login for Sagemaker SKLearn Image
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 683313688378.dkr.ecr.us-east-1.amazonaws.com
+# Login for building and pushing custom image
 aws ecr get-login-password --region $2 | docker login --username AWS --password-stdin $1.dkr.ecr.$2.amazonaws.com
 
 docker build -t $3 .
