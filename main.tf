c2fbe7aa9033b60fb72783b3c5d665d589ff4ac7
Sagar Sapkota <spkt.sagar@gmail.com>
2023-07-10 02:45:14 +0545

    update terraform and provider versions

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.7"
     }
     awscc = {
       source  = "hashicorp/awscc"
-      version = "~> 0.1"
+      version = "~> 0.55.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5.0"
 }
 
 variable "pipeline-name" {
